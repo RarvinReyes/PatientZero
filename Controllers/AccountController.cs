930d347a733055c60b3098492cbb6b5b4e575dae
Arvin Z Reyes <arvinzreyes@gmail.com>
2020-01-13 16:15:37 +0400

    initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PatientZero.Models;
+
+namespace PatientZero.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login() {
+
+            var user = new User();
+            return View(user);
+        }
+
+        public ActionResult Authenticate() {
+            HttpCookie cookie = new HttpCookie("username");
+
+            cookie.Value = "hey";
+
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PatientZero.MvcApplication" Language="C#" %>
